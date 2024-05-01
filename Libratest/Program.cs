42878d7837fe59ef61c93114fcbd1a41ca1aba3e
Katalina <47336744+Katalinaishere@users.noreply.github.com>
2024-05-01 12:08:21 +0200

    Add project files.

---
+++ Libratest/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+var allBooks = new List<Libratest.Books>()
+{
+    new Libratest.Books()
+    {
+        Title = "Book 1"
+    },
+    new Libratest.Books()
+    {
+        Title = "book2 "
+    }
+};
+
+
+foreach (var book in allBooks)
+{
+    Console.WriteLine(book.Title);
+}
+
+Console.ReadLine();
